3744a6ba68d368c90cc7fa6d130b4ee3f75aa9b9
Ana Cuco <ana.cuco@rocket-internet.de>
2020-01-24 15:15:13 +0100

    Adding elements

--- components/CountdownTimer.js
+++ components/CountdownTimer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 class CountdownTimer extends Component {
@@ -29,8 +29,13 @@ class CountdownTimer extends Component {
     const count = this.state.count;
 
     return (
-      <View>
-        <Text style={styles.container}>Stop talking in {count} seconds</Text>
+      <View style={styles.container}>
+        <Text style={styles.number}>{count}</Text>
+        <Text style={styles.text}>seconds</Text>
+        <View style={styles.clock}>
+          <View style={styles.seconds} />
+          <View style={styles.pointer} />
+        </View>
       </View>
     )
   }
@@ -38,7 +43,37 @@ class CountdownTimer extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#2c343a',
+    textAlign: 'center',
+  },
+  text: {
     color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  number: {
+    color: '#fff',
+    fontSize: 120,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  clock: {
+    borderWidth: 5,
+    borderColor: '#394349',
+    borderRadius: 350/2,
+    height: 350,
+    width: 350,
+  },
+  seconds: {
+    backgroundColor: '#e63b09',
+    width: 200,
+    height: 200,
+    borderRadius: 200/2
+  },
+  pointer: {
+    backgroundColor: '#fff',
+    height: 5,
+    width: 300,
   },
 });
 
