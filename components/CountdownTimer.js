efa8ccbabec9e7aa5634605804c5b787c42984fe
Ana Cuco <ana.cuco@rocket-internet.de>
2020-04-17 16:20:47 +0200

    moving pointer and logo with times

--- components/CountdownTimer.js
+++ components/CountdownTimer.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet, Image, Button} from 'react-native';
-import { ProgressCircle } from 'react-native-svg-charts'
+import {View, Text, StyleSheet, Image, Button, Animated, Easing} from 'react-native';
+import { ProgressCircle } from 'react-native-svg-charts';
+const DURATION = 6000;
 
 class CountdownTimer extends Component {
+  spinValue = new Animated.Value(0);
+
   constructor (props) {
     super(props);
 
@@ -40,23 +43,37 @@ class CountdownTimer extends Component {
         progress: 1 - (prevState.count - 0.1) / from
       }))
     }, 100);
+
+    Animated.timing(this.spinValue, {
+      toValue: 1,
+      duration: DURATION,
+      easing: Easing.linear,
+      useNativeDriver: true
+    }).start();
   };
 
   render () {
     const count = this.state.count;
     const progress = this.state.progress;
     const seconds = Math.round(count);
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
 
     return (
       <View style={styles.container}>
         <Text style={styles.number}>{seconds}</Text>
         <Text style={styles.text}>seconds</Text>
         <View style={styles.clock}>
-          <View style={styles.pointer} />
-          <Image
-            style={styles.logo}
-            source={require('../assets/logo.png')}
-          />
+          <Animated.View style={{transform: [{rotate: spin}] }}>
+            <View style={[styles.pointer]} />
+            <Image
+              style={styles.logo}
+              source={require('../assets/logo.png')}
+            />
+          </Animated.View>
+
         </View>
         <ProgressCircle
           style={{ height: 420}}
@@ -107,17 +124,16 @@ const styles = StyleSheet.create({
   },
   pointer: {
     backgroundColor: '#fff',
-    height: 3,
-    left: 118,
+    height: 100,
+    left: -5,
     position: 'absolute',
-    top: 108,
-    transform: [{ rotate: '-90deg'}],
-    width: 100,
+    marginTop: -13,
+    width: 6,
   },
   logo: {
     position: 'absolute',
-    top: 30,
-    left: 147,
+    top: 80,
+    left: -20,
     height: 40,
     width: 40,
   },
